e5dcf1ffe0e2b341894bfb95c5e0ee065a0fa303
“freddyscs” <frrp86@gmail.com>
2025-07-11 01:31:42 +0200

    terraform cloud

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,3 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "mi-organizacion"
+
+    workspaces {
+      name = "mi-workspace"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+resource "aws_instance" "mail_server" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MailServer"
+  }
 }
